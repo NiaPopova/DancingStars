a93981b341cd3524a83f1ca487a7f08bd64be5a4
Nia <fn45622@g.fmi.uni-sofia.bg>
2024-06-03 15:38:13 +0300

    removed import

--- DancingStars/src/main/java/com/dancing/stars/repository/TeamRepository.java
+++ DancingStars/src/main/java/com/dancing/stars/repository/TeamRepository.java
@@ -1,6 +1,5 @@
 package com.dancing.stars.repository;
 
-import com.dancing.stars.entity.Jury;
 import com.dancing.stars.entity.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
 
