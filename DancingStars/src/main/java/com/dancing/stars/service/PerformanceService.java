d78c3c4fb179d16dd1907948364f00824cc6b247
Nia <fn45622@g.fmi.uni-sofia.bg>
2024-06-03 15:04:09 +0300

    Refactoring

--- DancingStars/src/main/java/com/dancing/stars/service/PerformanceService.java
+++ DancingStars/src/main/java/com/dancing/stars/service/PerformanceService.java
@@ -4,7 +4,7 @@ import com.dancing.stars.entity.Performance;
 import com.dancing.stars.repository.PerformanceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
+
 
 import java.util.List;
 
