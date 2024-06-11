ae8432f854964db00381c012ba3d987ae543a690
Nikol Topozlijska <ntopozlijska18@gmail.com>
2024-06-11 11:39:03 +0300

    Episode Temaplte changes

--- DancingStars - FE/dancing-stars-fe/src/components/EpisodeTemplate.js
+++ DancingStars - FE/dancing-stars-fe/src/components/EpisodeTemplate.js
@@ -24,6 +24,42 @@ const EpisodeTemplate = ({ episode }) => {
                 )}
             </div>
 
+            {
+                episode.secondWinnerTeam && (
+                    <div className="winner-team">
+                        <h3>Second Winner Team</h3>
+                        <div className="team-details">
+                            {episode.secondWinnerTeam.photo && (
+                                <img src={require(`../assets/${episode.secondWinnerTeam.photo}`)} alt={`${episode.secondWinnerTeam.participantName} and ${episode.secondWinnerTeam.professionalName}`} />
+                            )}
+                            <p>Participant: {episode.secondWinnerTeam.participantName}</p>
+                            <p>Profession: {episode.secondWinnerTeam.participantProfession}</p>
+                            <p>Professional: {episode.secondWinnerTeam.professionalName}</p>
+                            <p>SMS: {episode.secondWinnerTeam.sms}</p>
+                            <p>Final Position: {episode.secondWinnerTeam.finalPosition}</p>
+                        </div>
+                    </div>
+                )
+            }
+
+            {
+                episode.thirdWinnerTeam && (
+                    <div className="winner-team">
+                        <h3>Third Winner Team</h3>
+                        <div className="team-details">
+                            {episode.thirdWinnerTeam.photo && (
+                                <img src={require(`../assets/${episode.thirdWinnerTeam.photo}`)} alt={`${episode.thirdWinnerTeam.participantName} and ${episode.thirdWinnerTeam.professionalName}`} />
+                            )}
+                            <p>Participant: {episode.thirdWinnerTeam.participantName}</p>
+                            <p>Profession: {episode.thirdWinnerTeam.participantProfession}</p>
+                            <p>Professional: {episode.thirdWinnerTeam.professionalName}</p>
+                            <p>SMS: {episode.thirdWinnerTeam.sms}</p>
+                            <p>Final Position: {episode.thirdWinnerTeam.finalPosition}</p>
+                        </div>
+                    </div>
+                )
+            }
+
             <div className="losing-team">
                 <h3>Losing Team</h3>
                 {episode.losingTeam && (
@@ -39,6 +75,27 @@ const EpisodeTemplate = ({ episode }) => {
                     </div>
                 )}
             </div>
+
+            {
+                episode.secondLosingTeam && (
+                    <div className="losing-team">
+                        <h3>Second Losing Team</h3>
+                        {episode.secondLosingTeam && (
+                            <div className="team-details">
+                                {episode.secondLosingTeam.photo && (
+                                    <img src={require(`../assets/${episode.secondLosingTeam.photo}`)} alt={`${episode.secondLosingTeam.participantName} and ${episode.secondLosingTeam.professionalName}`} />
+                                )}
+                                <p>Participant: {episode.secondLosingTeam.participantName}</p>
+                                <p>Profession: {episode.secondLosingTeam.participantProfession}</p>
+                                <p>Professional: {episode.secondLosingTeam.professionalName}</p>
+                                <p>SMS: {episode.secondLosingTeam.sms}</p>
+                                <p>Final Position: {episode.secondLosingTeam.finalPosition}</p>
+                            </div>
+                        )}
+                    </div>
+
+                )
+            }
         </div>
     );
 };
